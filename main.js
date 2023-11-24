410b6ed791f10b2e5a31a81c95e81169e8fb02da
antoniodalterio <antoniodalterio2227@gmail.com>
2023-11-24 15:10:34 +0100

    feat: bonus

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: 'Salve a tutti',
+      message: "L'amichevole Spider-Man di quartiere",
+      img: 'marvel.jpg',
+      classImg: 'sizing',
+      classText: 'red',
     };
   },
 }).mount('#app');
